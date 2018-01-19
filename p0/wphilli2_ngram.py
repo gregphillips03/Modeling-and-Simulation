de9c89150d7d09cf2ad26ac76a92b282bd9d3556
Greg Phillips <greg.phillips@aecom.com>
2018-01-19 16:48:59 -0500

    added in some initial stuff:

--- p0/wphilli2_ngram.py
+++ p0/wphilli2_ngram.py
@@ -2,3 +2,16 @@
 # William (Greg) Phillips
 # Working
 
+# --------------------------- #
+# --- Section 0 - Imports --- #
+# --------------------------- #
+
+from collections import Counter;
+import random;
+
+# ------------------------------------- #
+# --- Section 1 - Clean up the text --- #
+# ------------------------------------- #
+
+
+
