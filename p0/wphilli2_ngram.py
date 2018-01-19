3a353dae04437324aea86fba34f3e7b1f75778ba
Greg Phillips <greg.phillips@aecom.com>
2018-01-19 17:13:03 -0500

    section 2 exporatory

--- p0/wphilli2_ngram.py
+++ p0/wphilli2_ngram.py
@@ -8,10 +8,25 @@
 
 from collections import Counter;
 import random;
+import nltk; 
+import string; 
 
-# ------------------------------------- #
-# --- Section 1 - Clean up the text --- #
-# ------------------------------------- #
+# ---------------------------------------- #
+# --- Section 1 - Function Defintiions --- #
+# ---------------------------------------- #
 
+def get_tokens():
+	with open('corpus.txt', 'r') as shake:
+		text = shake.read();
+		lower = text.lower();
+		kill_punctuation = lowers.translate(None, string.punctuation); 
+		tokens = nltk.word_tokenize(kill_punctuation); 
+		return tokens; 
 
+# ------------------------------- #
+# --- Section 2 - Exploratory --- # 
+# ------------------------------- #
 
+tokens = get_tokens(); 
+count = Counter(tokens); 
+print count.most_common(10); 
